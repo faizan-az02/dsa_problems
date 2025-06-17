b8441b301eb1ab67a9773b6a6f2316f5e279b232
Faizan Aziz <faizan.professional02@gmail.com>
2025-06-17 13:46:01 +0500

    Third Problem

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ Return the maximum difference. If no such i and j exists, return -1.
 Problem 2:<br>
 You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
 Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i].
-Return the smallest index i at which either a row or a column will be completely painted in mat.
+Return the smallest index i at which either a row or a column will be completely painted in matrix.
+
+Problem 3:<br>
+You are given a map of a server center, represented as a m * n integer matrix grid, where 1 means that on that cell there is a server and 0 means that it is no server. Two servers are said to communicate if they are on the same row or on the same column.
+Return the number of servers that communicate with any other server.
---
+++ p3_server_count.py
@@ -0,0 +1,52 @@
+def countServers(grid):
+
+    rows, cols = len(grid), len(grid[0])
+
+    visited = set()
+
+    servers = set()
+
+    for i in range(rows): 
+        
+        for j in range(cols):
+
+            if grid[i][j] == 1:
+
+                servers.add((i, j))
+
+    for server in servers:
+
+        neighbor_found = 0
+
+        r, c = server
+
+        for i in range(rows):
+
+            if grid[i][c] == 1 and (i, c) != server:
+
+                neighbor_found = 1
+
+                visited.add((i, c))
+
+        for i in range(cols): 
+
+            if grid[r][i] == 1 and (r, i) != server:
+
+                neighbor_found = 1
+
+                visited.add((r, i))
+        
+        if neighbor_found == 1:
+
+            visited.add(server)
+
+    return len(visited)
+
+if __name__ == "__main__":
+
+    grid = [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]
+
+    print(countServers(grid))
+
+
+
